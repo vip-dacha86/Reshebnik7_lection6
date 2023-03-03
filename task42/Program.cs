2a902eae981195aaf0eae9c44679853f044c7105
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-03 17:45:03 +0500

    task42_perevo2sistem

---
+++ task42/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу которая будет преобразовывать десятичное число в двоичное.
+// 45->101101  3-> 11   2->10
+int number = int.Parse(Console.ReadLine());
+
+
+string res = DecToNum(number, 2);
+
+Console.WriteLine($"{number}->{res}");
+
+
+//Универсальный математический для перевода из 10 в любую
+string DecToNum(int decNumber, int otherSystem)
+{
+    string res = "";
+    string nums = "0123456789ABCDEF";
+    while (decNumber > 0)
+    {
+        int ost = decNumber / otherSystem;
+        res = nums[decNumber - otherSystem * ost] + res;
+        decNumber /= otherSystem;
+    }
+    return res;
+}
