c2c0a675bdf7a02f6cdd2b43a9e4611d6f61aca8
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-03 20:12:50 +0500

    task45_copy_massiv

---
+++ task45/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу которая будет создавть копию заданного массива с помощью поэлементного копирования
+int[]array =GetArray(10,0,10);
+Console.WriteLine(String.Join(" ",array));
+//  int[]CopyArray = CopyArray(array);
+Console.WriteLine(String.Join(" ,CopyArray(array)"));
+int[]GetArray(int size,int minValue, int maxValue)
+{
+    int[]res = new int[size];
+    for(int i = 0;i < size;i++)
+    {
+        res[i] =new Random().Next(minValue,maxValue+1);
+    }
+    return res;
+}
+int[]CopyArray(int[]inArray)
+{
+    int[]result = new int[inArray.Length];
+    for(int i = 0;i < inArray.Length;i++)
+    {
+        result[i] = inArray[i];
+
+    }
+    return result;
+}
